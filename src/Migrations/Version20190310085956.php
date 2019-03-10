a7e1eae54081f25e14ab0088b1019ba0e72ca7dd
Matej Kuna <mat.kuna@gmail.com>
2019-03-10 10:08:02 +0100

    use one migration varsion

--- src/Migrations/Version20190310085956.php
+++ src/Migrations/Version20190310085956.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190309172332 extends AbstractMigration
+final class Version20190310085956 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20190309172332 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE restaurant (id INT UNSIGNED AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, cuisine VARCHAR(255) NOT NULL, description VARCHAR(1024) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE restaurant (id INT UNSIGNED AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, cuisine VARCHAR(255) DEFAULT NULL, description VARCHAR(1024) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE restaurant_opening_interval (id INT UNSIGNED AUTO_INCREMENT NOT NULL, restaurant_id INT UNSIGNED NOT NULL, day_in_week SMALLINT NOT NULL, open_at TIME NOT NULL, close_at TIME NOT NULL, INDEX IDX_BA887A07B1E7706E (restaurant_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE restaurant_opening_interval ADD CONSTRAINT FK_BA887A07B1E7706E FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE');
     }
